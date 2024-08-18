d9a1898e07af9e819d4fef9643d6551cb0ef678f
OussamaAtifii <oussamaati03@gmail.com>
2024-08-18 21:28:05 +0200

    add fonts

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import { Aclonica, Lusitana } from "next/font/google";
+
+export const aclonica = Aclonica({ weight: "400", subsets: ["latin"] });
+
+// export const lusitana = Lusitana({
+//   subsets: ["latin"],
+//   weight: ["400", "700"],
+// });
