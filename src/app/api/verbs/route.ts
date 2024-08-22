7c41c0b599df7c0b058bc8d4317e5d351398af4b
OussamaAtifii <oussamaati03@gmail.com>
2024-08-23 00:45:50 +0200

    feat: get random verbs

--- src/app/api/verbs/route.ts
+++ src/app/api/verbs/route.ts
@@ -1,6 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
 import { verbs } from "./data";
+import { Verb } from "@/app/types/verb";
 
-export async function GET() {
-  const verbsName = verbs.map(({ name }) => ({ name }));
-  return Response.json(verbsName);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const quantity = searchParams.get("quantity");
+
+  if (quantity == "true") {
+    let quantityVerbs = [] as Verb[];
+
+    for (let i = 0; i < 20; i++) {
+      let randomIndex = Math.floor(Math.random() * verbs.length);
+
+      while (quantityVerbs.includes(verbs[randomIndex])) {
+        randomIndex = Math.floor(Math.random() * verbs.length);
+      }
+
+      quantityVerbs.push(verbs[randomIndex]);
+    }
+
+    return NextResponse.json(quantityVerbs);
+  }
+
+  return Response.json(verbs);
 }
