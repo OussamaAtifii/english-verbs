56af081868d45e1bda91e4d06e2f264a4c05782e
OussamaAtifii <oussamaati03@gmail.com>
2024-08-24 01:40:02 +0200

    feat: add skeleton and loading

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -1,75 +1,26 @@
 "use client";
 
-import Link from "next/link";
-import { SquareArrowLeft01Icon } from "../icons/icons";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import VerbData from "../components/verb/VerbData";
 import { Verb } from "../types/verb";
+import VerbSkeleton from "../components/verb/VerbSkeleton";
 
 export default function VerbDetails({ params }: { params: { name: string } }) {
   const { name } = params;
   const [verb, setVerb] = useState<Verb | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchVerb = async () => {
       const response = await fetch(`/api/verbs/${name}`);
       const data = await response.json();
-      console.log(data);
+
       setVerb(data);
+      setLoading(false);
     };
 
     fetchVerb();
   }, [name]);
 
-  return (
-    <>
-      <div className="flex flex-col justify-between min-h-[100dvh] p-6 bg">
-        <div className="flex flex-col gap-10 items-center">
-          {/* Encabezado con el nombre del verbo */}
-          <h1 className="text-5xl font-extrabold text-jellybean-950 uppercase underline tracking-wide">
-            {verb?.name}
-          </h1>
-
-          {/* Tarjetas con los detalles del verbo */}
-          <div className="w-full space-y-6">
-            <div className="bg-white shadow-lg rounded-xl p-5 border-l-4 border-jellybean-500">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Pasado Simple
-              </h2>
-              <p className="text-gray-700 mt-1 font-bold text-lg">
-                {verb?.past}
-              </p>
-            </div>
-            <div className="bg-white shadow-lg rounded-xl p-5 border-l-4 border-jellybean-500">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Pasado Participio
-              </h2>
-              <p className="text-gray-700 mt-1 font-bold text-lg">
-                {verb?.pastParticiple}
-              </p>
-            </div>
-            <h2 className="text-2xl font-bold text-gray-800 text-center pt-10">
-              Significado/s
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-              {verb?.meaning.map((mean) => (
-                <div
-                  className="bg-white shadow-lg  rounded-xl p-5 border-l-4 border-jellybean-500"
-                  key={mean}
-                >
-                  <h3 className="text-lg font-bold text-gray-800 ">{mean}</h3>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <Link
-          href="/"
-          className="bg-jellybean-500 hover:bg-jellybean-600 px-10 py-4 rounded-lg flex justify-center items-center gap-3 mt-8"
-        >
-          <SquareArrowLeft01Icon className="w-6 h-6" />
-          <span className="text-xl font-bold">Go back</span>
-        </Link>
-      </div>
-    </>
-  );
+  return loading ? <VerbSkeleton /> : <VerbData verb={verb} />;
 }
