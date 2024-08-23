fbcb0137a2aec8d35faf0b2b443290b339a2a10a
OussamaAtifii <oussamaati03@gmail.com>
2024-08-24 01:41:12 +0200

    improve navbar styles

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,7 +1,10 @@
 "use client";
+
 import Link from "next/link";
 import { useState } from "react";
-import { poppins } from "../fonts/fonts";
+import { bebasNeue } from "../fonts/fonts";
+import { BurgerIcon, Home03Icon, Quiz01Icon } from "../icons/icons";
+import NavLink from "./NavLink";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,58 +14,30 @@ export default function NavBar() {
   };
 
   return (
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 sticky top-0 bg-white border-b-2 border-gray-300 z-20">
       <Link href="/" className="flex items-center">
         <img src="/logo.webp" className="h-14" alt="VerbIt logo" />
         <span
-          className={` ${poppins.className} self-center text-2xl font-semibold whitespace-nowrap dar:text-white`}
+          className={`${bebasNeue.className} text-3xl font-semibold whitespace-nowrap`}
         >
           VerbIt
         </span>
       </Link>
       <button
-        data-collapse-toggle="navbar-default"
         type="button"
-        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-        aria-controls="navbar-default"
-        aria-expanded="false"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
         onClick={handleClick}
       >
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
+        <BurgerIcon />
       </button>
       {isOpen && (
-        <div className="w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+        <div className="w-full">
+          <ul className="font-medium text-lg flex flex-col pt-8 space-y-2 border-gray-100 rounded-md">
             <li>
-              <Link
-                href="/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dar:text-white md:dar:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </Link>
+              <NavLink href="/" icon={<Home03Icon />} name="Home" />
             </li>
             <li>
-              <Link
-                href="/quiz"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dar:text-white md:dar:hover:text-blue-500 dar:hover:bg-gray-700 dar:hover:text-white md:dar:hover:bg-transparent"
-              >
-                Quiz
-              </Link>
+              <NavLink href="/quiz" icon={<Quiz01Icon />} name="Quiz" />
             </li>
           </ul>
         </div>
