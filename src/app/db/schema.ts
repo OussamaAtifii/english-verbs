51a605dff2fa5044f7028805a06871750b8b0d33
OussamaAtifii <oussamaati03@gmail.com>
2024-09-03 00:06:03 +0200

    refactor: Update verbs table schema to make 'name' column unique

--- src/app/db/schema.ts
+++ src/app/db/schema.ts
@@ -1,12 +1,12 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const verbs = sqliteTable("verbs", {
+export const verbsTable = sqliteTable("verbs", {
   id: integer("id").primaryKey(),
-  name: text("name").notNull(),
+  name: text("name").unique().notNull(),
   past: text("past").notNull(),
-  pastParticiple: text("past_participle").unique().notNull(),
+  pastParticiple: text("past_participle").notNull(),
   meaning: text("meaning").notNull(),
 });
 
-export type InsertVerb = typeof verbs.$inferInsert;
-export type SelectVerb = typeof verbs.$inferSelect;
+export type InsertVerb = typeof verbsTable.$inferInsert;
+export type SelectVerb = typeof verbsTable.$inferSelect;
